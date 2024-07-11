7a12c94b294960b57a710b8a31cb4561c4495319
Vechkanova <vechkanova.p@gmail.com>
2024-07-11 18:13:27 +0400

    Добавлено создание базы данных и обработка при входе пользователя

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, request
+import hashlib
+import sqlite3
+from flask import Flask, render_template, redirect, session, url_for, request
 
 # Flask - библиотека для запуска нашего приложения Flask - app
 # render_template - нужен для то чтобы ваша страница html отобразилась корреткно
@@ -7,6 +9,15 @@ from flask import Flask, render_template, redirect, url_for, request
 # request - обработчик запросов GET/POST и дргуих
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  # подствавьте свой секретный ключ
+# секретный ключ для хеширования данных сессии при авторизации
+
+
+# Устанавливаем соединение с Базой Данных
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 #  наша корневая страиницу лендинда
@@ -19,19 +30,53 @@ def home():
 # страница формы логина в админ панель
 @app.route("/adm_login", methods=["GET", "POST"])
 def admin_login():
+    error = None # обнуляем переменную ошибок 
     if request.method == "POST":
-        # Здесь должна быть логика аутентификации
-        # Если аутентификация прошла успешно, перенаправляем на /admin_panel
-        return redirect(url_for("admin_panel"))
-    # Если GET запрос, показываем форму входа
-    return render_template("login_adm.html")
+        username = request.form[
+            "username"
+        ]  # обрабатываем запрос с нашей формы который имеет атрибут name="username"
+        password = request.form[
+            "password"
+        ]  # обрабатываем запрос с нашей формы который имеет атрибут name="password"
+        hashed_password = hashlib.sha256(
+            password.encode("utf-8")
+        ).hexdigest()  # шифруем пароль в sha-256
+
+        # устанавливаем соединение с БД
+        conn = get_db_connection()
+        # создаем запрос для поиска пользователя по username,
+        # если такой пользователь существует, то получаем все данные id, password
+        user = conn.execute(
+            "SELECT * FROM users WHERE username = ?", (username,)
+        ).fetchone()
+        # закрываем подключение БД
+        conn.close()
+
+        # теперь проверяем если данные сходятся формы с данными БД
+        if user and user["password"] == hashed_password:
+            # в случае успеха создаем сессию в которую записываем id пользователя
+            session["user_id"] = user["id"]
+            # и делаем переадресацию пользователя на новую страницу -> в нашу адимнку
+            return redirect(url_for("admin_panel"))
+        else:
+            error = "Неправильное имя пользователя или пароль"
+
+    return render_template("login_adm.html", error=error)
 
 
 # страница админ панели
 @app.route("/admin_panel")
 def admin_panel():
-    # Загрузка и отображение админ-панели
+    if "user_id" not in session:
+        return redirect(url_for("admin_login"))
     return render_template("admin_panel.html")
+
+@app.route('/logout')
+def logout():
+    # Удаление данных пользователя из сессии
+    session.clear()
+    # Перенаправление на главную страницу или страницу входа
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
---
+++ createuser.py
@@ -0,0 +1,24 @@
+import sqlite3
+import hashlib
+
+
+def create_user(username, password):
+    # Хеширование пароля
+    hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+    # Подключение к нашей базе данных
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+
+    # Добавление нового пользователя
+    c.execute(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        (username, hashed_password),
+    )
+
+    # Сохранение изменений и закрытие соединения с базой данных
+    conn.commit()
+    conn.close()
+
+
+create_user("admin", "your_password")
