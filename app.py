f4c6804fb5a01692dcd04e86bf62478e5d1a1531
Vechkanova <vechkanova.p@gmail.com>
2024-07-11 17:34:05 +0400

    Добавлены методы главной страницы, админ панели и входа в админ панель

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+
+# Flask - библиотека для запуска нашего приложения Flask - app
+# render_template - нужен для то чтобы ваша страница html отобразилась корреткно
+# redirect - нам понадобится для обработки запросы формы где мы перенаприм пользователя на страницу админ панели
+# url_for - вспомогательна библиотека для того чтобы сделать правильный переход по ссылке в нашем случеш мы будем ссылаться на adm_panel
+# request - обработчик запросов GET/POST и дргуих
 
 app = Flask(__name__)
 
 
+#  наша корневая страиницу лендинда
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def home():
+    # Загрузка и отображение главной страницы (landing page)
+    return render_template("landing.html")
 
 
-if __name__ == "main":
+# страница формы логина в админ панель
+@app.route("/adm_login", methods=["GET", "POST"])
+def admin_login():
+    if request.method == "POST":
+        # Здесь должна быть логика аутентификации
+        # Если аутентификация прошла успешно, перенаправляем на /admin_panel
+        return redirect(url_for("admin_panel"))
+    # Если GET запрос, показываем форму входа
+    return render_template("login_adm.html")
+
+
+# страница админ панели
+@app.route("/admin_panel")
+def admin_panel():
+    # Загрузка и отображение админ-панели
+    return render_template("admin_panel.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
